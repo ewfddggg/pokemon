504f076507ba49d2139cdf0702a6a9f5f5a216b3
ewfddggg <inecaev233@gmail.com>
2024-09-18 21:30:51 +0300

    r0 создание первого покемона

---
+++ pokemon3/pokemon3/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static pokemon3.pokemon;
+
+namespace pokemon3
+{
+    class pokemon
+    {
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int SpecialAttack { get; set; }
+        public int SpecialDefense { get; set; }
+        public int Speed { get; set; }
+
+
+        public pokemon(string name, int hp, int attack, int defense, int specialAttack, int specialDefense, int speed)
+        {
+            Name = name;
+            HP = hp;
+            Attack = attack;
+            Defense = defense;
+            SpecialAttack = specialAttack;
+            SpecialDefense = specialDefense;
+            Speed = speed;
+
+            
+        }
+
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine($"Имя: {Name}");
+            Console.WriteLine($"Очки здоровья: {HP}");
+            Console.WriteLine($"Атака: {Attack}");
+            Console.WriteLine($"Защита: {Defense}");
+            Console.WriteLine($"Специальная атака: {SpecialAttack}");
+            Console.WriteLine($"Специальная защита: {SpecialDefense}");
+            Console.WriteLine($"Скорость: {Speed}");
+         
+        }
+
+        public class Tornadus : pokemon
+        {
+            public Tornadus() : base("Tornadus", 45, 52, 60, 43, 60, 42 )
+            {
+            }
+        }
+
+ 
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tornadus tornadus = new Tornadus();
+
+
+            tornadus.PrintInfo();
+            Console.WriteLine();
+            Console.WriteLine();
+ 
+        }
+    }
+
+    
+}
